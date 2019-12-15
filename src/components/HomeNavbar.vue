75968baa15db9dbde5daa9d98bddc5a583d5aa67
Sagar <sagar1309@live.com>
2019-12-15 17:29:31 +0530

    adds navbar and sidebar components

---
+++ src/components/HomeNavbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar app color="primary" dense dark class="home-navbar">
+    <v-toolbar-title>{{ appName }} Billing System</v-toolbar-title>
+    <v-spacer></v-spacer>
+
+    <v-menu offset-y>
+      <template v-slot:activator="{ on }">
+        <v-btn icon v-on="on">
+          <v-icon>fas fa-user-circle</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item @click="$emit('logout', true)">
+          <v-list-item-title>Logout</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  name: "HomeNavbar",
+  props: ["appName"],
+  methods: {
+    //
+  }
+};
+</script>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-navigation-drawer app expand-on-hover permanent>
+    <v-list-item>
+      <v-list-item-avatar>
+        <v-icon>fas fa-user-circle</v-icon>
+      </v-list-item-avatar>
+
+      <v-list-item-title>{{ userDetails.username }}</v-list-item-title>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list dense>
+      <v-list-item v-for="item in items" :key="item.title" color="primary" class="my-3" link :to="{ name: item.route }">
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  name: "NavigationDrawer",
+  props: {
+    items: {
+      default: () => [],
+      type: Array
+    }
+  },
+  data() {
+    return {
+      //
+    };
+  },
+  computed: {
+    userDetails() {
+      return this.$store.getters.userDetails;
+    }
+  },
+  methods: {
+    //
+  }
+};
+</script>
