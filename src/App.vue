bd77b906777ea5596ad70853a4663c6d72e218ee
Sagar <sagar1309@live.com>
2019-12-15 17:42:47 +0530

    fix typo

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default {
 </script>
 <style lang="scss">
 #app {
-  font-family: "Raleway", sans-serif;
+  font-family: "Roboto", sans-serif;
   // -webkit-font-smoothing: antialiased;
   // -moz-osx-font-smoothing: grayscale;
   // text-align: center;
