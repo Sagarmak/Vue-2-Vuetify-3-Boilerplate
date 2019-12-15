bcc5bb139b59b5ea7b208fd5605a7a6c3cb92d50
Sagar <sagar1309@live.com>
2019-12-15 16:47:55 +0530

    Adds pre commit lint checking using husky, adds Roboto font, fontawesome in index and an .env file

--- src/App.vue
+++ src/App.vue
@@ -10,23 +10,11 @@
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Roboto", sans-serif;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+  padding: 0;
+  margin: 0;
 }
 </style>
